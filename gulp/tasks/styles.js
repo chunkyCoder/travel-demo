6991bbb851711d0a88c45aa1742afddd0149e8e2
= <evoisin+c9@outlook.com>
2017-08-15 19:43:35 +0000

    modify top half of page with responsive css

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,5 +1,6 @@
 var gulp          = require("gulp"),
     postcss       = require("gulp-postcss"),
+    mixins        = require("postcss-mixins"),
     cssImport     = require("postcss-import"),
     autoprefixer  = require("autoprefixer"),
     cssVars       = require("postcss-simple-vars"),
@@ -7,7 +8,7 @@ var gulp          = require("gulp"),
 
 gulp.task("styles", function() {
     return gulp.src("./app/styles/styles.css")
-    .pipe(postcss([cssImport,cssVars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssVars, nested, autoprefixer]))
     .on("err", function(errInfo) {
         console.log(errInfo.toString());
         this.emit("end");  
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,12 +2,7 @@ var gulp          = require("gulp"),
     watch         = require("gulp-watch");
 
 
-
 gulp.task("watch", function() {
-   watch("./app/index.html", function() {
-       gulp.start("html");
-   });
-   
    watch("./app/styles/**/*.css", function() {
        gulp.start("styles");
    });
